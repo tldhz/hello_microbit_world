63cc457937b8b1bb49243e5831106caefdc279d7
tldhz <tldhz@users.noreply.github.com>
2023-02-08 12:37:37 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+})
+basic.forever(function () {
+	
+})
